2e6f03fd875a6a98b61e44b4783685ccb1b433ba
CrisisCore-Systems <CrisisCore.Systems@proton.me>
2024-12-11 18:43:11 -0800

    feat: add basic project structure and tests

---
+++ crisiscore/__init__.py
@@ -0,0 +1,7 @@
+"""
+CrisisCore Systems Image Processor
+--------------------------------
+A professional-grade image processing suite with cyberpunk aesthetics.
+"""
+
+__version__ = '0.1.0'
---
+++ tests/test_basic.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestBasicSetup(unittest.TestCase):
+    def test_environment(self):
+        """Test that the test environment is working"""
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
